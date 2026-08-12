e24fd18dcbaa9aaecd64c1bdfd398b9fca17c23a
Daniel <weberdk01@gmail.com>
2026-08-12 11:52:56 +0000

    ci: setup simple dev container

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     tsconfigPaths: true,
   },
